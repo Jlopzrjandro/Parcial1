770c8dbe830ad868753945ea5372f5b21fab9bce
Jlopzrjandro <110216865+Jlopzrjandro@users.noreply.github.com>
2022-08-08 20:26:18 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="contenedor">
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="tabla.php">Tablas</a></li>
+                <li><a href="#">Formulario</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <main class="contenedor1">
+        <div class="contenedor2">
+            <div class="img">
+                <img src="computadora.jpg" alt="">
+            </div>
+            <div class="texto">
+                <h1 class="titulo">Bienvenido</h1>
+                <p class="parrafo">Al examen parcial de desarrollo Web, en las páginas que usted verá aquí podrá ver el avance que hemos tenido en las primeras semanas de clase...</p>
+            </div>
+        </div>
+    </main>
+    
+    <footer class="footer">
+        <h2 class="titulo2">Todos los derechos reservados a /UMG/</h2>
+    </footer>
+</body>
+</html>
